8f7dcfed20e63796530b6f45136eee470901c932
Tewodros <thedronberhanu05@gmail.com>
2025-02-20 16:19:08 +0300

    second set Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={18000}
+          height={380}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
