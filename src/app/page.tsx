bd4956f28bbcaa0d968559d6b7c672cdca639b8b
Tewodros <thedronberhanu05@gmail.com>
2025-02-21 23:24:27 +0300

    background customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Go to nextjs.org →
+            Go to nextjs.org → Official website
           </a>
         </footer>
       </div>
