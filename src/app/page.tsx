fb95154c1c44a9e2ba93aef8f2a7c11e26e96e67
Tewodros <thedronberhanu05@gmail.com>
2025-03-01 10:52:10 +0300

    card Modification 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import './globals.css';
 export default function Home() {
   return (
     <div className="relative min-h-screen overflow-hidden bg-black text-white">
-      {/* Particle Background */}
+      {/* Particle Background from code pen*/}
       <ParticlesBackground />
 
       <div className="relative z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
