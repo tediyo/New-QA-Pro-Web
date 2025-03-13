587e67f06e46bdd2c2ec11f936df747adb4bac2d
Tewodros <thedronberhanu05@gmail.com>
2025-03-13 17:02:07 +0300

    taileind version execution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,3 +76,8 @@ export default function Home() {
     </div>
   );
 }
+
+// export default function Page() {
+//   return <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>;
+// }
+
