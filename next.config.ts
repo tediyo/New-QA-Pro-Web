67978a785374fb6bc9a522ab84d4f0fa328cd2f8
Tewodros <thedronberhanu05@gmail.com>
2025-03-21 10:32:09 +0300

    API integration for crypto news

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 // next.config.js
-module.exports = {
-  images: {
-    domains: ['randomuser.me'], // Add the domain where the image is hosted
-  },
+// module.exports = {
+//   images: {
+//     domains: ['randomuser.me'], // Add the domain where the image is hosted
+//   },
   
     
-}
+// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.cryptodaily.co.uk",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
